c4bdd6276c55dce35db4b4b0c20eeec30e274c05
EnstromJohan <112412351+EnstromJohan@users.noreply.github.com>
2022-09-09 09:11:31 +0200

    Add project files.

---
+++ hemsidegenerator/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+string start = "<!DOCTYPE html>\n<html>\n<body\n<main>";
+string welcome = "<h1>\nVälkomna!\n</h1>";
+string kurser = "Kurs om C#\n Kurs om databaser";
+string end = "</body>\n</html>";
+
+Console.WriteLine($"{start}\n{welcome}\n{kurser}\n{end}");
