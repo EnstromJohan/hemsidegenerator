1f996f9fc9e3daf9ecc7aebace4dd732fde0cc1e
EnstromJohan <112412351+EnstromJohan@users.noreply.github.com>
2022-09-09 09:37:46 +0200

    Ändrat menyn

--- hemsidegenerator/Program.cs
+++ hemsidegenerator/Program.cs
@@ -1,9 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-string start = "<!DOCTYPE html>\n<html>\n<body\n<main>";
-string welcome = "<h1>\nVälkomna!\n</h1>";
-string kurser = "Kurs om C#\n Kurs om databaser";
+// DATA FRÅN API
+string[] techniques = {"C#", "daTAbaser", "WebbuTVeCkling", "clean Code"};
+
+
+//STATSIK DATA
+string start = "<!DOCTYPE html>\n<html>\n<body\n<main>\n";
+string welcome = "<h1>\nVälkomna!\n</h1>\n";
+string kurser = "";
+
+foreach (string technique in techniques)
+{
+    string tmp = technique.Trim();
+  
+    kurser += "<p>" + tmp[0].ToString().ToUpper() + tmp.Substring(1).ToLower() + "</p>\n";
+
+
+}
+
+
+
 string end = "</body>\n</html>";
 
-Console.WriteLine($"{start}\n{welcome}\n{kurser}\n{end}");
+
+Console.WriteLine($"{start}{welcome}{kurser}{end}");
+
+//string kurser = "Kurs om C#\n Kurs om databaser";
